457e8494356cabde3e56fd092651857ebef4ca68
庞孟臣 <pangmengchen@deepexi.com>
2020-08-03 09:41:14 +0800

    feat: 顶部菜单改为左侧菜单
    Signed-off-by: fizz <pangmengchen@deepexi.com>

--- config/environment.js
+++ config/environment.js
@@ -95,7 +95,7 @@ module.exports = function(environment) {
       projectToken: '%PROJECTID%',
 
       // apiServer: 'https://localhost:30443',
-      apiServer: 'https://10.57.4.2',
+      apiServer: 'https://openkube.deepexi.cloud',
       apiEndpoint: '/v3',
       publicApiEndpoint: '/v3-public',
       clusterEndpoint: '/v3/clusters/%CLUSTERID%',
